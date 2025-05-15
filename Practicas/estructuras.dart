223eeb9349a89ae80c76059210afc9d7d2dc6eed
Yuver-10 <antoniodjromana@gmail.com>
2025-05-15 09:30:26 -0500

    Comentario de un documento

--- Practicas/estructuras.dart
+++ Practicas/estructuras.dart
@@ -1,49 +1,49 @@
-void main() {
-  mapExample();
-}
+// void main() {
+//   mapExample();
+// }
 
-void mapExample() {
-  List<String> numbers = ["UNo", "Dos", "Tres"];
+// void mapExample() {
+//   List<String> numbers = ["UNo", "Dos", "Tres"];
 
-  var numbers2 = ["Cuatro", "Cinco", "Seis"];
-  var numbers3 = ["Cuatro", "Cinco", "Seis"];
+//   var numbers2 = ["Cuatro", "Cinco", "Seis"];
+//   var numbers3 = ["Cuatro", "Cinco", "Seis"];
 
-  numbers[0] = "Uno Modificado";
-  numbers.add("Siete");
-  // numbers.addAll(numbers2, numbers3);
-  numbers.remove("Siete");
-  numbers.insert(0, "Yuver Martinez");
-  // numbers.sort();
-  // numbers.clear();
-  print(numbers);
+//   numbers[0] = "Uno Modificado";
+//   numbers.add("Siete");
+//   // numbers.addAll(numbers2, numbers3);
+//   numbers.remove("Siete");
+//   numbers.insert(0, "Yuver Martinez");
+//   // numbers.sort();
+//   // numbers.clear();
+//   print(numbers);
 
-  // Imprimir los datos de la lista recorriendo el contenido
-  for (var i = 0; i < numbers.length; i++) {
-    print(numbers[i]);
-  }
+//   // Imprimir los datos de la lista recorriendo el contenido
+//   for (var i = 0; i < numbers.length; i++) {
+//     print(numbers[i]);
+//   }
 
-  // Sets listas con valores no repetidos
-  void setExample() {
-    Set<String> numbers = {"Uno", "Dos", "Tres"};
-    var numbers2 = {"Dos", "Cuatro"};
-  }
+//   // Sets listas con valores no repetidos
+//   void setExample() {
+//     Set<String> numbers = {"Uno", "Dos", "Tres"};
+//     var numbers2 = {"Dos", "Cuatro"};
+//   }
 
-  numbers.add("Siete");
-  // numbers2.addAll(numbers2);
-  numbers.remove("Dos");
-  numbers.clear();
+//   numbers.add("Siete");
+//   // numbers2.addAll(numbers2);
+//   numbers.remove("Dos");
+//   numbers.clear();
 
-  for (var i = 0; i < numbers.length; i++) {
-    print(numbers.elementAt(i));
-  }
+//   for (var i = 0; i < numbers.length; i++) {
+//     print(numbers.elementAt(i));
+//   }
 
-  print("numbers");
+//   print("numbers");
 
-  void mapExample() {
-    Map<String, int> numbers = {"Uno": 1, "Tres": 3};
-    numbers["Cuatro"] = 4;
-    numbers.remove("Uno");
-    numbers.removeWhere((key, value) => key == "Tres");
-    print(numbers);
-  }
-}
+//   void mapExample() {
+//     Map<String, int> numbers = {"Uno": 1, "Tres": 3};
+//     numbers["Cuatro"] = 4;
+//     numbers.remove("Uno");
+//     numbers.removeWhere((key, value) => key == "Tres");
+//     print(numbers);
+//   }
+// }
